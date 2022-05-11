c1585969a16de8f0eafbf842a8aed92532547992
WangLei <wangl_msg@163.com>
2022-05-11 10:24:41 +0800

    测试git

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello1 <a href="https://nextjs.org">Next.js!</a>
+          Hello <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
